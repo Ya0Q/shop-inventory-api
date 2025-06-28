8ed833168fafe78a2aeed92842a53727defb2d92
Xin Qiu <xq1@williams.edu>
2025-06-28 01:46:53 -0500

    added api testing to CI pipeline

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ const app = express();
 app.use(express.json());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 
+app.get('/openapi.json', (req, res) => {
+  res.json(specs);
+});
+
 // Root route
 /**
  * @swagger
